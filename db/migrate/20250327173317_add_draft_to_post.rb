005bf076506c93e3d17861ff2b527ead361d9cc7
Lucas Borges da Silva <lucas18sborges@gmail.com>
2025-03-27 14:37:53 -0300

    MODEL: Add draft column to posts table

---
+++ db/migrate/20250327173317_add_draft_to_post.rb
@@ -0,0 +1,5 @@
+class AddDraftToPost < ActiveRecord::Migration[8.0]
+  def change
+    add_column :posts, :draft, :boolean, default: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_07_031107) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_27_173317) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -57,6 +57,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_07_031107) do
     t.integer "user_id"
     t.string "spotify_track_url"
     t.string "slug"
+    t.boolean "draft", default: true
     t.index ["slug"], name: "index_posts_on_slug", unique: true
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
