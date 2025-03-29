918b123a868de466a3b4a57900459b239cf8f6fa
Lucas Borges <lucas.borges@maino.com.br>
2025-03-28 22:12:50 -0300

    STYLE: Adiciona correções de estilização de texto do rubocop

--- test/system/posts_test.rb
+++ test/system/posts_test.rb
@@ -3,17 +3,17 @@ require "application_system_test_case"
 class PostsTest < ApplicationSystemTestCase
   setup do
     @user = users(:one)
-    
+
     # Criar um post válido com ActionText::RichText
     @post = Post.new(
       title: "Post de Teste para System Test",
       user: @user
     )
-    
+
     # Configurando o rich_text content corretamente
     @post.content = ActionText::Content.new("Este é o conteúdo de teste para o system test, precisa ter mais de 10 caracteres")
     @post.save!
-    
+
     # Fazer login como usuário
     system_login_as(@user)
   end
@@ -30,11 +30,11 @@ class PostsTest < ApplicationSystemTestCase
     click_on "Novo Post"
 
     fill_in "Titulo", with: "Novo Post via System Test"
-    
+
     # Interagir com o editor Trix
     find("trix-editor").click
     find("trix-editor").set("Este é o conteúdo do novo post via system test")
-    
+
     click_on "Create Post"
 
     assert_text "Post was successfully created"
@@ -45,11 +45,11 @@ class PostsTest < ApplicationSystemTestCase
     click_on "Editar", match: :first
 
     fill_in "Titulo", with: "Post Atualizado via System Test"
-    
+
     # Interagir com o editor Trix
     find("trix-editor").click
     find("trix-editor").set("Este é o conteúdo atualizado via system test")
-    
+
     click_on "Update Post"
 
     assert_text "Post was successfully updated"
