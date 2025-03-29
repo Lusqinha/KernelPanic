0671beea452914cf38f9c70831af288d46c49dbe
Lucas Borges <lucas.borges@maino.com.br>
2025-03-28 21:56:18 -0300

    TESTE: Adiciona método de login no sistema e ajusta testes de posts

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,4 +2,12 @@ require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ]
+
+  def system_login_as(user)
+    visit "/admin"
+    fill_in "E-mail", with: user.email_address
+    fill_in "Senha", with: "password"
+    click_button "Entrar"
+    assert_selector "#admin-btn"
+  end
 end
